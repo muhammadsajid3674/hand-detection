6c6a4fdc565aa63fdb67833b6bf8ec5cf85dd31a
muhammadsajid3674 <sajiddark3674@gmail.com>
2023-07-12 01:04:01 +0500

    not completed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-// 1. Install dependencies DONE
-// 2. Import dependencies DONE
-// 3. Setup webcam and canvas DONE
-// 4. Define references to those DONE
-// 5. Load handpose DONE
-// 6. Detect function DONE
-// 7. Drawing utilities DONE
-// 8. Draw functions DONE
-
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 // import logo from './logo.svg';
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
 import "./App.css";
+import useWindowSize from "./hook";
 // import { drawHand } from "./utilities.js";
 // import ringImg from "./img/ring.png";
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  const [isLoading, setLoader] = useState(true)
   const imgURI = window.location.search
-  let newStr = imgURI.substring(1);
+  let newStr = imgURI.split("==");
+  let imgType = newStr[1];
+  let imgUrl = newStr[3];
 
-  // window.location.href += imgURI
-  console.log('URL :>> ', newStr);
+  const size = useWindowSize();
   const runHandpose = async () => {
     try {
       const net = await handpose.load();
@@ -32,6 +26,8 @@ function App() {
       setInterval(() => {
         detect(net);
       }, 100);
+      setLoader(false)
+
     } catch (error) {
       console.log('fetch error :>> ', error);
     }
@@ -48,10 +44,9 @@ function App() {
       const video = webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
-
       // Set video width
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
+      webcamRef.current.video.width = window.innerWidth;
+      webcamRef.current.video.height = window.innerHeight;
 
       // Set canvas height and width
       canvasRef.current.width = videoWidth;
@@ -59,61 +54,70 @@ function App() {
 
       // Make Detections
       const hand = await net.estimateHands(video);
-      console.log(hand);
-      console.log('width :>> ', webcamRef.current.video);
-      console.log('height :>> ', canvasRef.current.height);
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
-      ctx.rect(canvasRef.current.width / 2 - 250, canvasRef.current.height / 2 - (window.innerWidth < 600 ? 75 : 200), 500, (window.innerWidth < 600 ? 150 : 400));
+      ctx.rect(
+        0,
+        0,
+        280,
+        480
+      );
       ctx.fillStyle = "transparent";
       ctx.fill();
       ctx.lineWidth = 4;
       ctx.strokeStyle = hand.length > 0 ? "green" : "red";
       ctx.stroke();
+
       // drawHand(hand, ctx);
       if (ctx.strokeStyle === '#008000') {
         var img = document.getElementById("ringImg");
-        ctx.drawImage(img, canvasRef.current.width / 2 - 20, canvasRef.current.height / 2 - (window.innerWidth < 600 ? 35 : 100), 40, (window.innerWidth < 600 ? 25 : 50));
+        if (imgType === 'ring') {
+          ctx.drawImage(img, canvasRef.current.width / 2 - 20, canvasRef.current.height / 2 - 100, 40, 50);
+        } else if (imgType === 'braclet') {
+          ctx.drawImage(img, canvasRef.current.width / 2 - 40, canvasRef.current.height / 2 - 25, 80, 50);
+        }
       }
     }
   };
 
   runHandpose();
-
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header" style={{ position: 'relative' }}>
-        <Webcam
-          className="webCam"
-          ref={webcamRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            height: 700,
-          }}
-        />
+        {isLoading ? <div className="basic"></div> : <>
+          <Webcam
+            className="webCam"
+            ref={webcamRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+            }}
+            videoConstraints={{
+              width: { ideal: 280 },
+              height: { ideal: 480 },
+              facingMode: 'enviroment'
+            }}
+          />
 
-        <canvas
-          id="myCanvas"
-          className="canvas-width"
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            height: 700,
-          }}
-        />
-        <img id="ringImg" src={newStr} alt="The Scream" style={{ display: 'none' }}></img>
+          <canvas
+            id="myCanvas"
+            className="canvas-width"
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+            }}
+          />
+          <img id="ringImg" src={imgUrl} alt="The Scream" style={{ display: 'none' }}></img>
+        </>}
       </header>
     </div>
   );
